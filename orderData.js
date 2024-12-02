5bbcaa19e164ccfbdc95fc749e55a51bebdf33c8
Ajilalitha <ajilalitha010@gmail.com>
2024-12-02 21:18:27 +0530

    added

---
+++ orderData.js
@@ -0,0 +1,50 @@
+const orderDetailsDate = [
+    {
+        title : "Aloo tikki",
+        img : "aloo tikki.jpg",
+        price : "120",
+        rating: 4
+    },
+    {
+        title : "Aloo puri",
+        img : "aloo puri.jpg",
+        price : "67",
+        rating: 3.5
+    },
+    {
+        title : "Paneer",
+        img : "paneer.jpg",
+        price : "89",
+        rating: 4.7
+    },
+    {
+        title : "Parata",
+        img : "paratha.jpg",
+        price : "45",
+        rating: 4.2
+    },
+    {
+        title : "Uttapam",
+        img : "uttapam.jpg",
+        price : "40",
+        rating: 3.9
+    },
+    {
+        title : "Vada pav",
+        img : "vada pav.jpg",
+        price : "34",
+        rating: 4.1
+    },
+    {
+        title : "Uttapam",
+        img : "uttapam.jpg",
+        price : "40",
+        rating: 3.9
+    },
+    {
+        title : "Vada pav",
+        img : "vada pav.jpg",
+        price : "34",
+        rating: 4.1
+    },
+]
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {orderDetailsData} from './orderData.js';
 
 const menuBtn = document.querySelector(".mobileMenu");
 const mobileNavigation = document.querySelector(".mobileNavigation")
@@ -13,3 +14,34 @@ menuBtn.addEventListener("click",()=>{
         openNavigation = true
      }
 })
+
+
+const orderProductList = document.querySelector(".list_of_items_order")
+
+let orderProductCard = ''
+
+for(let i = 0; i < orderDetailsData.length ; i++){
+   orderProductCard += `
+   <div class="card">
+                    <img src="./assets/${orderDetailsData[i].img}"/>
+                    <div class="star">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+                    <div class="description">
+                        <div class="title">${orderDetailsData[i].title}</div>
+                        <div class="price_button">
+                          <p>${orderDetailsData[i].price}</p>
+                          <button class="btn">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+   `
+}
+
+orderProductList.innerHTML = 
+console.log(orderDetailsData);
+
